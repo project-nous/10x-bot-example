d6fe112934fd28dd8c5396fd393c5c0c78881014
Jordan Mackie <jordan@projectvault.co>
2022-03-09 12:17:27 +0000

    Enable strict typechecking

--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -1,7 +1,10 @@
 import middy from "@middy/core";
 import middyJsonBodyParser from "@middy/http-json-body-parser";
 import httpHeaderNormalizer from "@middy/http-header-normalizer";
+import {
+  Handler as LambdaHandler,
+} from 'aws-lambda'
 
-export const middyfy = (handler) => {
+export const middyfy = (handler: LambdaHandler) => {
   return middy(handler).use(middyJsonBodyParser()).use(httpHeaderNormalizer());
 };
